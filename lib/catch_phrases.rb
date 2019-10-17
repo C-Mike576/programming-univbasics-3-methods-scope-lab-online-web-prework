4086d28241c706f08666c23fe7d9eac1a12e6fe4
Michael Calkins <m.calkins576@gmail.com>
2019-10-17 14:26:59 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,5 +7,8 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
 def link
-  
+  starting = "It's Dangerous To Go Alone! Take This."
+  puts starting
+end
